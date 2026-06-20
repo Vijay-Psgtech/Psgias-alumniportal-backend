509050a2cdf6500730b93f00666e636d0fa2f835
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-06-20 12:16:10 +0530

    Albums module complete files has been added.

---
+++ models/Album.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const albumSchema = new mongoose.Schema(
+    {
+        year: { type: Number, required: true },
+        id: { type: String, required: true, unique: true },
+        title: { type: String, required: true },
+        event: { type: String, required: true },
+        date: { type: String, required: true },
+        photos: { type: Number, default: 0 },
+        accent: { type: String, default: "#b8882a"},
+        tags: [String],
+        images: [String],
+    },{ timestamps: true }
+);
+
+module.exports = mongoose.model("Album", albumSchema);
